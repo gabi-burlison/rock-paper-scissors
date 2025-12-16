94e5666d53ad275dc8a3c911abd6a52487f46be9
Gabriela Burlison <gabrielasiqueira014@gmail.com>
2025-12-16 12:49:03 -0900

    Add event listener to each button and play a 5 round game

--- index.js
+++ index.js
@@ -1,50 +1,35 @@
-//Clean code: 1) Replace var by const and/or let; 2) Validate the user's input: Cancel; 2.1) Validate the input as being one of the 3 allowed ones. 
+//Human choice
+const rockButton = document.querySelector("#rock");
+const paperButton = document.querySelector("#paper");
+const scissorsButton = document.querySelector("#scissors");
 
-let humanScore = 0; 
-let computerScore = 0; 
-function playRound() { 
-       do {
-        //Prompt
-        let input = prompt("Let's play Rock, Paper, or Scissors! Please, type your choice:"); 
+rockButton.addEventListener("click", () => playRound("rock"));
+paperButton.addEventListener("click", () => playRound("paper"));
+scissorsButton.addEventListener("click", () => playRound("scissors"));
 
-        //Validate null inputs and restart
-        if (input === null) {
-        alert("You have cancelled the game.");
-        location.reload();
-        }
+let humanScore = 0;
+let computerScore = 0;
 
-        const caseInsensitive = input.charAt(0).toUpperCase() + input.slice(1).toLowerCase(); 
-        //Computer options
-        const computerOptions = ["Rock", "Paper", "Scissors"];
-        const randomIndex = Math.floor(Math.random() * computerOptions.length); 
-        const randomOption = computerOptions[randomIndex];   
-        let result = "";
-        
-        //Validate accepted plays
-        if (caseInsensitive!== "Rock"&&caseInsensitive!=="Paper"&&caseInsensitive!=="Scissors") {
-                alert("Please, submit a valid answer.");
-                continue;
-        } 
-
-        //5 round game      
-                if (caseInsensitive===randomOption) {
-                result = "It's a tie! Player II chose "+randomOption;
-        } else if ((caseInsensitive==="Rock"&&randomOption==="Scissors") || (caseInsensitive==="Paper"&&randomOption==="Rock") || (caseInsensitive==="Scissors"&&randomOption==="Paper")) {
-                result = "You won! Player II chose "+randomOption;
-                humanScore++;
-        } else {
-                result = "You lost! Player II chose "+randomOption;
-                computerScore++;
-               } 
-        alert(result);
-        alert("You have "+humanScore+" points, and Player II has "+computerScore+" points")
-
-} while (humanScore<5 && computerScore<5);
-       if (humanScore===5) {
-        alert("Congratulations! You won the game!"); 
-       } else if (computerScore===5) {
-        alert("You lost the game. Try again!");
-} location.reload(); //Restart game
+function playRound(humanChoice) {
+    //Computer choice
+    const computerOptions = ["rock", "paper", "scissors"];
+    const computerChoice = computerOptions[Math.floor(Math.random() * 3)];
+    let result = "";
+    if (humanChoice === computerChoice) {
+        result = "It's a tie! Try again."
+    } else if ((humanChoice === "rock" && computerChoice === "scissors") || (humanChoice === "paper" && computerChoice === "rock") || (humanChoice === "scissors" && computerChoice === "rock")) {
+        console.log("You won! " + humanChoice + " beats " + computerChoice);
+        humanScore++;
+    } else {
+        console.log("Computer won. " + computerChoice + " beats " + humanChoice);
+        computerScore++;
+    } if (humanScore === 5 && computerScore < 5) {
+        console.log("Congratulations! You won!");
+        alert("Play again?");
+        window.location.reload();
+    } else if (humanScore < 5 && computerScore === 5) {
+        console.log("You lost. Try again!");
+        alert("Play again?");
+        window.location.reload();
+    } console.log("Current score: You: " + humanScore + " Computer: " + computerScore);
 }
-
-playRound();
